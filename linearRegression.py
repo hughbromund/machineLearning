9ebc741899a67a84afb46b35e4d9b73309d5e969
hugh <hughbromund44@gmail.com>
2019-04-09 19:29:56 -0400

    imported some stuff

--- linearRegression.py
+++ linearRegression.py
@@ -1,1 +1,14 @@
+import numpy
+import keras
+from keras.datasets import boston_housing
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
 print("TEST")
+
+(x_train, y_train), (x_test, y_test) = boston_housing.load_data()
